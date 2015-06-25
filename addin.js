b96d592aab07c5631a9d152a2e5b2be9f02936e2
sumurthy <suramam@microsoft.com>
2015-06-24 18:30:06 -0700

    Update and Delete

--- addin.js
+++ addin.js
@@ -1,35 +1,16 @@
 
-(function() {
-  var shopifyProductsAllAPI = "http://localhost:4567/products?callback=?";
-  		$.ajaxSetup({dataType: "jsonp" });
-		$.getJSON(shopifyProductsAllAPI)
-		 .done(function(json) {
-		 	console.log( "JSON Data: " + json[0].title );
-		 })
-		  .fail(function( jqxhr, textStatus, error ) {
-		   		var err = textStatus + ", " + error;
-		  	console.log( "Request Failed: " + err );
-});
-})();
 
-
-// $( document ).ready(function() {
- 
-    
-// 	$("#useJSONP").click(function(){
-// 		alert( "Starting ajax call" );
-//         $.ajax({
-//             url: 'http://localhost:4567/products',
-//             dataType: 'jsonp',
-//             jsonp: 'callback',
-//             jsonpCallback: 'jsonpCallback',
-//             success: function(){
-//                 alert("success");
-//             }
-//         });
-//     });
- 
+// // Get all products
+// (function() {
+//   var shopifyProductsAllAPI = "http://localhost:4567/products?callback=?";
+//   		$.ajaxSetup({dataType: "jsonp" });
+// 		$.getJSON(shopifyProductsAllAPI)
+// 		 .done(function(json) {
+// 		 	console.log( "JSON Data: " + json[0].title );
+// 		 })
+// 		  .fail(function( jqxhr, textStatus, error ) {
+// 		   		var err = textStatus + ", " + error;
+// 		  	console.log( "Request Failed: " + err );
 // });
+// })();
 
-// function jsonpCallback(data){
-//     $('#jsonpResult').text(data.message);
--- test.rb
+++ test.rb
@@ -21,12 +21,65 @@ get '/products' do
   # process the params however you want
     #shop = ShopifyAPI::Shop.current
     products = ShopifyAPI::Product.find(:all)
-    
+
     callback = params['callback']
     content_type :js    
-    content_type 'application/javascript'
+    #content_type 'application/javascript'
     response['Access-Control-Allow-Origin'] = 'http:localhost:4567'
 
     "#{callback}(#{products.to_json})"
 end
 
+post '/update' do
+  # process the params however you want
+    #shop = ShopifyAPI::Shop.current
+    retStr = ''
+    outData = ''
+    request.body.rewind
+    data = JSON.parse(request.body.read)
+    data["products"].each do |product|
+        retStr =  "............" + retStr + product["id"].to_s + product["price"] + product["inventory_quantity"].to_s + product["title"] + "............"
+        pID = product["id"]
+        tempProduct = ShopifyAPI::Product.find(pID)
+        outData = outData + '----------' +  tempProduct.title + " - - " + pID.to_s + '---------- \n' 
+        tempProduct.title = product["title"]
+        tempProduct.variants[0].inventory_quantity = product["inventory_quantity"]
+        tempProduct.variants[0].price = product["price"]
+        tempProduct.save    
+    end    
+    # retStr = retStr + '\n' + outData
+    # content_type 'text/plain'
+    # response['Access-Control-Allow-Origin'] = 'http:localhost:4567'
+    # retStr
+    products = ShopifyAPI::Product.find(:all)
+    callback = params['callback']
+    content_type :js    
+    #content_type 'application/javascript'
+    response['Access-Control-Allow-Origin'] = 'http:localhost:4567'
+    "#{callback}(#{products.to_json})"
+
+end
+
+
+post '/delete' do
+  # process the params however you want
+    #shop = ShopifyAPI::Shop.current
+    retStr = ''
+    outData = ''
+    request.body.rewind
+    data = JSON.parse(request.body.read)
+    data["products"].each do |product|
+        retStr =  "............" + retStr + product["id"].to_s + product["price"] + product["inventory_quantity"].to_s + product["title"] + "............"
+        pID = product["id"]
+        tempProduct = ShopifyAPI::Product.find(pID)
+        outData = outData + '----------' +  tempProduct.title + " - - " + pID.to_s + '---------- \n' 
+        tempProduct.destroy  
+    end    
+    # retStr
+    products = ShopifyAPI::Product.find(:all)
+    callback = params['callback']
+    content_type :js    
+    #content_type 'application/javascript'
+    response['Access-Control-Allow-Origin'] = 'http:localhost:4567'
+    "#{callback}(#{products.to_json})"
+end
